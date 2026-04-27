b17e94452db254e3386af3efd280094c782a9a8e
Robert Cowher <bobcowher@gmail.com>
2026-04-26 21:54:29 -0500

    up dated validation script

--- test.py
+++ test.py
@@ -1,12 +1,7 @@
 from agent import Agent
 import gymnasium as gym
-import ale_py
-from life_penalty_wrapper import LifePenaltyWrapper
 
-gym.register_envs(ale_py)
-
-env = gym.make("ALE/Breakout-v5", render_mode="rgb_array")
-env = LifePenaltyWrapper(env, penalty=-1.0)
+env = gym.make("CarRacing-v3", continuous=False, render_mode="human")
 
 agent = Agent(env=env)
 
